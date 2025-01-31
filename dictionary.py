76ede74551f94068989d99d0e5cb7e1df9e36f37
tdoiron0 <trentwdoiron@gmail.com>
2025-01-30 21:07:33 -0500

    ...

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# dict = {"dogs": {"name": "Jerremy", "age": 3},
+#         "cats": {"name": "Joe", "age": 5},
+#         "cars": {"name": "Bronco", "age": 25}}
+# for it in dict:
+#     print(dict[it]["name"])
+
+locations = {"food": (33.773728072939456, -84.39816428963466),
+             "school": (33.77475843185764, -84.39737313105833),
+             "park": (33.75955089299415, -84.3928687013215)}
+
+dict = {"Foo": "Bar",
+         "Up": "Down"}
+
+for it in dict:
+    print("a" in dict[it])
--- reading_tuples.py
+++ reading_tuples.py
@@ -1,3 +1,8 @@
 list = [("1", "2", "run"), ("2","1","walk")]
 for it in list:
     print(f"state_2: {it[1]}")
+
+cord_a = (5, 7)
+cord_b = (10, 13)
+temp = cord_a[0] - cord_b[0]
+print(temp)
